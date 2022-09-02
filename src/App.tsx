d7310de91be6f3efa3836ba0261bac3f2fb2aedd
davileao <davileao@gmail.com>
2022-09-02 13:51:38 -0300

    adding function to select itens

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,28 @@ import {Stopwatch} from "./components/Stopwatch";
 import {Itasks} from "./types/itasks";
 
 
+
+
 function App() {
   const [tasks, setTasks] = useState<Itasks[]>([])
+  const [selected, setSelected] = useState<Itasks>();
+
+  function selectTask (selectedTask: Itasks) {
+    setSelected(selectedTask);
+    setTasks(oldTasks => oldTasks.map(task => ({
+        ...task,
+        selected: task.id === selectedTask.id
+    })));
+
+  }
 
   return (
     <div className="AppStyle">
       <Form setTasks={setTasks}/>
-        <List  tasks={tasks}/>
+        <List
+            tasks={tasks}
+            selectTask={selectTask}
+        />
         <Stopwatch />
     </div>
   );
--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -3,13 +3,20 @@ import style from './list.module.scss';
 import Item from "./Item";
 import {Itasks} from "../../types/itasks";
 
-function List({tasks}: { tasks: Itasks[] }) {
+
+interface Iprops {
+    tasks: Itasks[],
+    selectTask: (selectedTask: Itasks) => void
+}
+
+function List({tasks, selectTask}: Iprops) {
     return (
         <aside className={style.taskList}>
             <h2 > Studies of the day </h2>
-            <ul> {tasks.map((item, index) => (
+            <ul> {tasks.map((item) => (
                 <Item
-                    key={index}
+                    selectTask={selectTask}
+                    key={item.id}
                     task={item.task}
                     time={item.time}
                     selected={item.selected}
